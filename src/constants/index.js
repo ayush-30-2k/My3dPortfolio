fe79ae76b852f79d56806c5e432328bc851a0619
ayush-30-2k <80163767+ayush-30-2k@users.noreply.github.com>
2024-12-22 15:58:49 +0530

    done

--- src/constants/index.js
+++ src/constants/index.js
@@ -147,38 +147,6 @@ export const socialLinks = [
 
 export const projects = [
   {
-    iconUrl: pricewise,
-    theme: "btn-back-red",
-    name: "Musify",
-    description:
-      "Created a music application that can provide whole kind of music format",
-    link: "https://github.com/adrianhajdin/pricewise",
-  },
-  {
-    iconUrl: threads,
-    theme: "btn-back-green",
-    name: "Full Stack Threads Clone",
-    description:
-      'Created a full-stack replica of the popular discussion platform "Threads," enabling users to post and engage in threaded conversations.',
-    link: "https://github.com/adrianhajdin/threads",
-  },
-  {
-    iconUrl: car,
-    theme: "btn-back-blue",
-    name: "Car Finding App",
-    description:
-      "Designed and built a mobile app for finding and comparing cars on the market, streamlining the car-buying process.",
-    link: "https://github.com/adrianhajdin/project_next13_car_showcase",
-  },
-  {
-    iconUrl: snapgram,
-    theme: "btn-back-pink",
-    name: "ToonChat - Chat Application",
-    description:
-      "Built a complete clone of Instagram, allowing users to share photos and connect with friends in a familiar social media environment.",
-    link: "https://toonchat.netlify.app",
-  },
-  {
     iconUrl: estate,
     theme: "btn-back-black",
     name: "Apple iPhone 3D - Three.js Web App",
@@ -193,5 +161,13 @@ export const projects = [
     description:
       "App that leverages AI to automatically generate concise & informative summaries from lengthy text content, or blogs.",
     link: "https://doraemon3d.netlify.app",
+  },
+  {
+    iconUrl: snapgram,
+    theme: "btn-back-pink",
+    name: "ToonChat - Chat Application",
+    description:
+      "Built a complete clone of Instagram, allowing users to share photos and connect with friends in a familiar social media environment.",
+    link: "https://toonchat.netlify.app",
   },
 ];
